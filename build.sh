9a87f2a92a1d3f03b2a8d315e3aba34d08f36704
Manuel Bua <manuel.bua@gmail.com>
2020-08-16 13:12:53 +0200

    Add linux/amd64 build

--- build.sh
+++ build.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 
 version="$(git describe --tags --long)"
+
 go build -o bin/tlscan -ldflags="-X 'github.com/manuelbua/tlscan/pkg/runner.version=${version}'" cmd/tlscan/main.go
+GOOS=linux GOARCH=amd64 go build -o bin/tlscan_linux64 -ldflags="-X 'github.com/manuelbua/tlscan/pkg/runner.version=${version}'" cmd/tlscan/main.go
+
 if [ "$?" = 0 ]; then
   echo "Built version ${version}"
 fi
